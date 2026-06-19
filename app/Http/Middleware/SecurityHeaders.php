c462bb626da2caaa0b00ec229a05f54874eecef1
steven tringali <dr.tringali@gmail.com>
2026-06-18 21:44:59 -0700

    alpine security header

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -33,7 +33,7 @@ class SecurityHeaders
         // unsafe-inline is required for Livewire/Alpine and Tailwind JIT inline styles
         $csp = implode('; ', [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline'",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com",
             "img-src 'self' data:",
